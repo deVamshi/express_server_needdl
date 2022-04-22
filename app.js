c07bc1265a371403c7a0e790910374fecef07886
deVamshi <dev.vamshidhar@gmail.com>
2022-04-22 15:43:38 +0530

    enabled cors

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const cors = require("cors");
 const connectDB = require("./db/connect");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const { dp } = require("./misc/helpers");
 const usersRoute = require("./routes/user");
 const notFound = require("./middleware/not-found");
 
+app.use(cors());
 app.use(express.json());
-
 app.use("/api/v1/user", usersRoute);
 
 app.get("/", (req, res) => {
